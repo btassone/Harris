f0e35421ba3732edcdb3b43e89db10deb56add78
Brandon Tassone <brandon@carrotcreates.com>
2015-02-27 23:49:56 -0500

    VC - 0.3.3 - Added checkbox clearing on car change. Added support for one graph and only field that works right now is speed. Data is being generated randomly but the controller parts are what count.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -24,14 +24,15 @@ harrisControllers.controller('HistoryCtrl', ['$rootScope', '$scope', '$http', 'C
     function($rootScope, $scope, $http, ChartFactory){
 
         $rootScope.activeLink = "history";
-
         $scope.cars = ChartFactory.cars;
+        $scope.clicked_car_fields = [];
+        $scope.chart_fields = [];
+        $scope.selected_car = null;
 
         $scope.clicked_car = function(car) {
             $scope.selected_car = car;
             $scope.getClickedCarFields(car);
 
-            console.log("Car Selection...");
             console.log($scope.selected_car);
         };
 
@@ -42,7 +43,25 @@ harrisControllers.controller('HistoryCtrl', ['$rootScope', '$scope', '$http', 'C
                     fields.push(cval);
             });
             $scope.clicked_car_fields = fields;
-        }
+
+            // Clear Chart Fields
+            $scope.chart_fields = [];
+            $(".field_boxes").each(function(){
+                $(this).removeAttr('checked');
+            });
+            $scope.charts = undefined;
+        };
+
+        $scope.clicked_field = function(field) {
+            var field_index = $scope.chart_fields.indexOf(field);
+            $scope.charts = true;
+            if(field_index == -1) {
+                $scope.chart_fields.push(field);
+                ChartFactory.generateChart($scope.selected_car, $scope.chart_fields);
+            } else {
+                $scope.chart_fields.splice(field_index, 1);
+            }
+        };
 }]);
 
 harrisControllers.controller('SettingsCtrl', ['$rootScope', '$scope', '$http',
