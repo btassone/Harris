fb5d71399a6b3c0d21cbf77408e27e8b60287783
Brandon Tassone <brandon@carrotcreates.com>
2015-03-12 03:19:02 -0400

    VC - 0.4.6.1 - Fixed a quick date issue in the rest_factory part.

--- services/rest_factory.js
+++ services/rest_factory.js
@@ -10,8 +10,8 @@ harrisApp.factory('RestFactory', ['$http', 'alert', 'alertNew', 'alertId', 'data
                 // TODO: Remove backup date ranges once date picker is added
                 var urlString = data + '?VIN=' + encodeURIComponent(vDataObj.vinString) +
                     '&data_elements=' + encodeURIComponent(vDataObj.propString) +
-                    '&start_time=' + encodeURIComponent(vDataObj.startTime || '2015-01-07T22:11:01.000Z') +
-                    '&end_time=' + encodeURIComponent(vDataObj.endTime || '2015-07-07T22:11:01.000Z');
+                    '&start_time=' + encodeURIComponent(vDataObj.startTime || '2015-03-01 00:00:00') +
+                    '&end_time=' + encodeURIComponent(vDataObj.endTime || '2015-03-07 23:59:59');
 
                 return $http.get(urlString);
             },
